bea7ea86c599b5c1ce1a9acccfa4d2409d2d9ab1
Mark Laane <MarkLaane@gmail.com>
2016-11-16 01:09:27 +0100

    refactored votes_controller

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -26,11 +26,9 @@ class VotesController < SecuredController
     registration = current.registrations.find_by order_no: params["choice"]
     raise "No registration found!" if registration.nil?
 
-    # Let's try to save the vote to DB
-    vote = Vote.new(call_params)
-    vote.active = true
-    vote.registration = registration
-    vote.save!
+    vote_params = call_params
+    vote_params[:registration] = registration
+    add_vote(vote_params)
 
     # All went well - let's notify user!
     @response = "Thank you! Vote received!"
@@ -48,20 +46,25 @@ class VotesController < SecuredController
     return head(:forbidden) unless current.running?
 
     registration = current.registrations.find_by order_no: params["Body"].to_i
-    raise "No registration found!" if registration.nil?
+    return head(:forbidden) if registration.nil?
 
-    # Let's try to save the vote to DB
-    vote = Vote.new(call_params)
-    vote.active = true
-    vote.registration = registration
-    vote.save!
-    current.votes
-        .where(from_phone: vote.from_phone)
-        .where.not(id: vote.id)
-        .update(active: false)
+    vote_params = call_params
+    vote_params[:registration] = registration
+    add_vote(vote_params)
+
   end
 
   private
+    def add_vote params
+      # Let's try to save the vote to DB
+      vote = Vote.new(params)
+      vote.active = true
+      vote.save!
+      Voting.current.votes
+          .where(from_phone: vote.from_phone)
+          .where.not(id: vote.id)
+          .update(active: false)
+    end
     def call_params
       {
           from_phone:   params['From'],
